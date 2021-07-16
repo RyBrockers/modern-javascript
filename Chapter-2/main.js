77840c624ef88808a0837251477177c01352d6e0
Ryan Brockley <rybrockers@yahoo.com>
2021-07-16 19:46:56 +0100

    variables - chapter-2

--- Chapter-2/main.js
+++ Chapter-2/main.js
@@ -1,6 +1,17 @@
 // alert("hello world")
+alert("Hello World")
+
 // the console log
+
 console.log("Ryan is awesome");
-// variables
+
+// variables that the value can change
 let age = 36;
 console.log(age);
+
+let year = "2021"
+console.log(year);
+
+// when the value of the variable does not change
+const name = "Ryan";
+console.log(name);
