7e706e55aa029c73939482c837634fc47ac9432c
Ryan Brockley <rybrockers@yahoo.com>
2021-07-16 20:14:58 +0100

    completed String basics.

---
+++ Chapter-3/main.js
@@ -0,0 +1,30 @@
+// Chapter 3 - Strings are a series of characters. They have methods as well as properties. 
+
+
+// Strings
+console.log("hello world");
+
+let email = "rybrockers@yahoo.com"
+console.log(email);
+//  String concatenation 
+let firstName = "Brandon";
+let lastName = "Sanderson";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Getting charactrs
+console.log(fullName[3]);
+
+// String length
+console.log(fullName.length);
+
+// String methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName); 
+
+// some methods do not alter the vatiable original value
+let index = email.indexOf("@");
+console.log(index);
+// however, some variables do
